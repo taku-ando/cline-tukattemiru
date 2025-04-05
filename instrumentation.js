1066378074d18e66ae3d37a46ff52893c49819d8
takuya ando <taku.ando.work@gmail.com>
2025-04-05 11:24:33 +0900

    mockはいったんOK

--- instrumentation.js
+++ instrumentation.js
@@ -1,7 +1,12 @@
 export async function register() {
+  // if (process.env.NEXT_RUNTIME === "nodejs") {
+  //   const { server } = await import("./msw/setup/server");
+  //   server.listen();
+  //   console.log("Mock server is running!");
+  // }
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { server } = await import("./mock/node");
-    server.listen();
+    const { initMocks } = await import("./msw/setup");
+    initMocks();
     console.log("Mock server is running!");
   }
 }
